2af28dc5b775857f03db823f498b1c7ba2dc2ba3
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-14 08:15:23 -0500

    proyect app scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '', //localhost:4200
-    loadChildren: () => import(`../app/modules/auth/auth.module`).then (m => m.AuthModule),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
